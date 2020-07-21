43a3e76533b37e0bd82b43ac006e2b0ae02591f3
Mark Chmarny <mark@chmarny.com>
2020-07-21 07:32:53 -0700

    added cron binding demo

---
+++ cron-binding/go.mod
@@ -0,0 +1,5 @@
+module github.com/mchmarny/cron-example
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ cron-binding/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.OPTIONS("/run", optionsHandler)
+	r.POST("/run", runHandler)
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
+
+func runHandler(c *gin.Context) {
+	// TODO: do something interesting here
+	log.Printf("invocation received: %v", time.Now())
+	c.JSON(http.StatusOK, nil)
+}
+
+func optionsHandler(c *gin.Context) {
+	c.Header("Allow", "POST,OPTIONS")
+	c.Header("Content-Type", "application/json")
+	c.AbortWithStatus(http.StatusOK)
+}
